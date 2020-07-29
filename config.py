35ed25ec0db814f75bf57a1d5ab1a6fa515c09e0
AngliD <mcrus.com@gmail.com>
2020-07-29 14:43:26 +0300

    Project: Add BASE_DIR constant and ngrok env for testing throw ngrok.

--- config.py
+++ config.py
@@ -1,8 +1,13 @@
+import pathlib
+
 from pydantic import BaseSettings
+
+BASE_DIR = pathlib.Path('.').parent.resolve()
 
 
 class Settings(BaseSettings):
     telegram_bot_token: str
+    ngrok_tunnel_address: str
 
     class Config:
         env_file = '.env'

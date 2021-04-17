2e090698921c33231fdeaa1f3518fc7d63d6bc7f
glavrab <g2NUYeda32200>
2021-04-18 01:57:54 +0300

    feat: Add func to check availability of command

--- telegram_bot/dependencies.py
+++ telegram_bot/dependencies.py
@@ -1,5 +1,4 @@
 import config
-
 from fastapi import HTTPException, status, Path
 
 
@@ -7,3 +6,12 @@ def verify_telegram_bot_token(telegram_bot_token: str = Path(...)):
     """Verify if token was presented in a given url as Path operator."""
     if telegram_bot_token != config.settings.telegram_bot_token:
         raise HTTPException(detail='Wrong Token', status_code=status.HTTP_403_FORBIDDEN)
+
+
+def check_if_command_available(command_in_message: str) -> bool:
+    """Check if command can be processed"""
+    available_commands = ['/skip', '/clear', '/list']
+    for command in available_commands:
+        if command == command_in_message:
+            return False
+        return True
